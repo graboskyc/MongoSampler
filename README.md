62a7a5005b4049bc0cdd84d6d098b728f47a408e
Chris Grabosky <chris@grabosky.net>
2019-04-17 19:16:52 -0400

    CLI flags

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ For performance considerations visit [the npm page](https://www.npmjs.com/packag
 
 ![](CompassScreenshot.png)
 
-# Use
+# Usage
+Basic Usage: 
+`graboskycMBP:MongoSampler graboskyc$ ./index.js --host mongodb+srv://username:passwd@host --db dbname --collection colname`
+
+Add the `--values` flag if you want to include values into the JSON output
+
+Use the `--sample` flag if instead of using all documents in the collection and instead use a random 1000 documents
+
+# Editing
 Edit `index.js` to put in database, collection, and connection string. Then run the `index.js` and the output will look similar to [SampleOut.json](SampleOut.json) for a given collection whose documents look like [SampleDoc.json](SampleDoc.json).
 
 The `removeValues` boolean will determine whether to pass back all possible values or not.
--- index.js
+++ index.js
@@ -3,11 +3,28 @@
 var mongodb = require('mongodb');
 const chalk = require('chalk');
 const parseSchema = require('mongodb-schema');
+const commandLineArgs = require('command-line-args')
 
-const dbName = "";
-const colName = "";
-const connstr = ""+dbName;
-const removeValues = true;
+var dbName = "";
+var colName = "";
+var connstr = ""+dbName;
+var keepValues = false;
+var sample = false;
+
+const optionDefinitions = [
+    { name: 'host', alias: 'h', type: String, description:"Connection string including mongdb://, username, password, no trailing /" },
+    { name: 'db', alias: 'd', type: String, description:"Database name" },
+    { name: 'collection', alias: 'c', type: String, description:"Collection name" },
+    { name: 'values', alias: 'v', type: Boolean, description:"If supplied, include values in response" },
+    { name: 'sample', alias: 's', type: Boolean, description:"If included only do a sample of 1000 docs" }
+  ]
+const options = commandLineArgs(optionDefinitions)
+
+if (options.hasOwnProperty('db')) { dbName = options.db; }
+if (options.hasOwnProperty('collection')) { colName = options.collection; }
+if (options.hasOwnProperty('host')) { connstr = options.host + "/" + dbName; }
+if (options.hasOwnProperty('values')) { keepValues = options.values; }
+if (options.hasOwnProperty('sample')) { sample = options.sample; }
 
 console.log(chalk.green('Connecting to: ', connstr));
 
@@ -30,11 +47,16 @@ mongodb.connect(connstr, { useNewUrlParser: true }, function(err, client){
         return process.exit(1);
     }
     var db = client.db(dbName);
+    var pipeline = [];
 
-    parseSchema(db.collection(colName).find(), function(err, schema) {
+    if(sample) {
+        pipeline.push({ '$sample': { 'size': 1000 }});
+    }
+
+    parseSchema(db.collection(colName).aggregate(pipeline), function(err, schema) {
         if (err) return console.error(chalk.red(err));
 
-        if(removeValues) { schema = walk(schema);}
+        if(!keepValues) { schema = walk(schema);}
 
         console.log(JSON.stringify(schema, null, 2));
         client.close();
